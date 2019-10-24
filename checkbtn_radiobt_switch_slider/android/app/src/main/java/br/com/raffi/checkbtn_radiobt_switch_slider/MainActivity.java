13d21d1a049d8f986726311c39a5299226b7e313
Eduardo <eduardo_raffi@hst.com.br>
2019-10-24 17:26:32 -0300

    Aula checkbox

---
+++ checkbtn_radiobt_switch_slider/android/app/src/main/java/br/com/raffi/checkbtn_radiobt_switch_slider/MainActivity.java
@@ -0,0 +1,13 @@
+package br.com.raffi.checkbtn_radiobt_switch_slider;
+
+import android.os.Bundle;
+import io.flutter.app.FlutterActivity;
+import io.flutter.plugins.GeneratedPluginRegistrant;
+
+public class MainActivity extends FlutterActivity {
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    GeneratedPluginRegistrant.registerWith(this);
+  }
+}
---
+++ checkbtn_radiobt_switch_slider/lib/main.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+import 'package:checkbtn_radiobt_switch_slider/EntradaCheckbBox.dart';
+
+void main() => runApp(MaterialApp(
+    home: EntradaCheckBox(),
+  )
+);
